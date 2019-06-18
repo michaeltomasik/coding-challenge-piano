e0e7c5d36c2532d66b779c1e01418fa28d52456c
michal <shugo12345@gmail.com>
2019-06-19 01:52:48 +0200

    :seedling: Add joyride

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import { Jumbotron, Button } from 'react-bootstrap';
+import Joyride from 'react-joyride';
 
 import Piano from './components/Piano';
 import SaveTitleModal from './components/SaveTitleModal';
@@ -19,6 +20,29 @@ class App extends React.Component {
       isRecordMode: false,
       showPopup: false,
       startSongDate: null,
+      showTutorial: false,
+      steps: [
+        {
+          target: '.circle',
+          content: 'Click Here to start Recording Your song',
+          disableBeacon: true,
+        },
+        {
+          target: '.Piano',
+          content: 'Play your favourite song by click on piano keys',
+          disableBeacon: true,
+        },
+        {
+          target: '.circle',
+          content: 'When you finish click stop button that will appear here',
+          disableBeacon: true,
+        },
+        {
+          target: '.SongList',
+          content: 'After you save your song it should apper in Song titles! To play it again simply press your song title.',
+          disableBeacon: true,
+        },
+      ],
     };
 
     this.pianoRef = React.createRef();
@@ -27,6 +51,7 @@ class App extends React.Component {
     this.switchRecordMode = this.switchRecordMode.bind(this);
     this.closeModal = this.closeModal.bind(this);
     this.playSong = this.playSong.bind(this);
+    this.handleJoyrideCallback = this.handleJoyrideCallback.bind(this);
   }
 
   addNote(note) {
@@ -63,8 +88,20 @@ class App extends React.Component {
     });
   }
 
+  handleJoyrideCallback = data => {
+    const { joyride } = this.props;
+
+    if (joyride && typeof joyride.callback === "function") {
+      joyride.callback(data);
+    } else {
+      if(data.action === 'reset') {
+        this.setState({ showTutorial: false });
+      }
+    }
+  };
+
   render() {
-    const { playedNotes, isRecordMode, showPopup } = this.state;
+    const { playedNotes, isRecordMode, showPopup, steps, showTutorial } = this.state;
     const pianoNotes = ['C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5'];
 
     return (
@@ -75,7 +112,11 @@ class App extends React.Component {
             <p>This is a simple piano app that allows you to record your songs and save them in database.</p>
             <p>Technologies: React, Graphql, Nodejs, Mongodb</p>
             <p>
-              <Button variant="primary">Learn more</Button>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  this.setState({ showTutorial: true })
+                }}>How to use it?</Button>
             </p>
           </div>
         </Jumbotron>
@@ -90,14 +131,29 @@ class App extends React.Component {
               pianoNotes={pianoNotes}
               addNote={this.addNote} />
               <div>
-                <div
-                  onClick={this.switchRecordMode}
-                  className="record"></div>
-                {isRecordMode ?
-                  'Stop Recording' :
-                  'Start Recording'}
+                <div className="circle">
+                {!isRecordMode ?
+                  <div>
+                    <div
+                      onClick={this.switchRecordMode}
+                      className="record"></div>
+                  </div> :
+                  <div>
+                    <div
+                      onClick={this.switchRecordMode}
+                      className="stop"></div>
+                  </div>}
+                </div>
+                {isRecordMode ? 'Recording...' : null}
               </div>
           </div>
+          <Joyride
+            steps={steps}
+            run={showTutorial}
+            callback={this.handleJoyrideCallback}
+            showSkipButton
+            continuous
+            scrollToFirstStep />
           <SaveTitleModal
             isOpen={showPopup}
             closeModal={this.closeModal}
